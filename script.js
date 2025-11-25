118c754d32372ceb08ff96aac9bdb513e6d228b1
sltan29 <almog.sultan29@gmail.com>
2025-11-25 14:37:24 +0200

    created a functional calculator, used a generic event listener for numbers and created a live display

---
+++ script.js
@@ -0,0 +1,74 @@
+
+let operator = undefined;
+let x=0;
+let y=0;
+
+let number = document.querySelectorAll('.number');
+let display = document.querySelector('.display');
+let add = document.querySelector('#add');
+let subtract = document.querySelector('#subtract');
+let multiply = document.querySelector('#multiply');
+let divide = document.querySelector('#divide');
+let clear = document.querySelector('#clear');
+let equal = document.querySelector('#equal')
+
+number.forEach((num) => {
+    num.addEventListener('click', function() {
+        let digit = parseInt(num.textContent);
+        if (operator===undefined)
+        {
+            x=x*10+digit;
+            display.textContent = x;
+        }
+        else
+        {
+            y=y*10+digit;
+            display.textContent = x  +  operator  + y;
+        }
+    })
+});
+
+add.addEventListener('click', function() {
+    operator = '+'
+    display.textContent += operator;
+})
+
+subtract.addEventListener('click', function() {
+    operator = '-'
+    display.textContent += operator;
+})
+
+divide.addEventListener('click', function() {
+    operator = '/'
+    display.textContent += operator;
+})
+
+multiply.addEventListener('click', function() {
+    operator = '*'
+    display.textContent += operator;
+})
+
+clear.addEventListener('click',function(){
+    resetAll();
+    display.textContent='';
+})
+
+equal.addEventListener('click',function() {
+    let result;
+    if (operator === '+')
+        result = x+y;
+    else if(operator === '-')
+        result = x-y;
+    else if (operator === '/')
+        result = x/y;
+    else if (operator ==='*')
+        result = x*y;
+    display.textContent=result;
+    resetAll();
+})
+
+function resetAll() {
+    x=0;
+    y=0;
+    operator=undefined;
+}
